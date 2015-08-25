81f9053416b218b29c7a3437290d158be4cdf963
Twister915 <joey.sacchini264@gmail.com>
2015-08-24 20:15:47 -0400

    that's about all for today

---
+++ src/StormNetwork/StormCore/Frontend/Player/PunishmentListener.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Electric
+ * Date: 8/24/2015
+ * Time: 8:07 PM
+ */
+
+namespace StormNetwork\StormCore\Frontend\Player;
+
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use StormNetwork\StormCore\Http\StormClient;
+
+class PunishmentListener implements Listener {
+    public function onPlayerLogin(PlayerLoginEvent $event) {
+        StormClient::sendData('GET', ['username' => $event->getPlayer()->getName(), 'ip' => $event->getPlayer()->getAddress()], 'punishments/targeted/noAuth', $this, function ($ca, $resp) {
+            if ($resp->code != 200) return;
+            $punishments = $resp->response;
+            foreach ($punishments as $punishment) {
+                $ca->handlePunishment($punishment);
+            }
+        });
+    }
+
+    private function handlePunishment($punishment) {
+        //todo implement punishment handler
+    }
+}
---
+++ src/StormNetwork/StormCore/Player/BasePermissible.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Electric
+ * Date: 8/24/2015
+ * Time: 8:15 PM
+ */
+
+namespace StormNetwork\StormCore\Player;
+
+
+class BasePermissible {
+
+}
